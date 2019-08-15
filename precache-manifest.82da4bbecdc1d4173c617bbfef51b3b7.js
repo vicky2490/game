163c31e7217d221b0172de3fa8f6708c3d5579c7
vicky <s353397@sinyi.com.tw>
2019-08-15 17:22:23 +0800

    Updates

---
+++ precache-manifest.82da4bbecdc1d4173c617bbfef51b3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eca7b0508af481768c376863798226b",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "d3d257cc213bc8844928",
+    "url": "/game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c5c322815729b3dd1cff",
+    "url": "/game/static/js/2.279f60d7.chunk.js"
+  },
+  {
+    "revision": "d3d257cc213bc8844928",
+    "url": "/game/static/js/main.52b1b54f.chunk.js"
+  },
+  {
+    "revision": "84dc501e979b43114feb",
+    "url": "/game/static/js/runtime~main.8e7b486d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game/static/media/logo.5d5d9eef.svg"
+  }
+]);
